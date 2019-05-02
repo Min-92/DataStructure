8c0e5b337980bd9f7eb0aa1c92138c56ad97efc0
Min-92 <okssud1@naver.com>
2019-05-02 11:06:43 +0900

    feat : add stack class

---
+++ stack.js
@@ -0,0 +1,21 @@
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+    push(item){
+        this.stack.push(item);
+    }
+    pop(){
+        return this.stack.pop();
+    }
+    size(){
+        return this.stack.length;
+    }
+    peek(){
+        return this.stack[this.stack.length-1];
+    }
+    isEmpty(){
+        return this.stack.length === 0 ? true : false;
+    }
+
+}
