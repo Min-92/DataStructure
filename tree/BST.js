87e59b73ffdec54205ceb7b5f8e6f9c317757f48
Min-92 <0201.mj.lee@gmail.com>
2019-08-24 17:39:58 +0900

    feat : erase method 추가

--- tree/BST.js
+++ tree/BST.js
@@ -6,6 +6,7 @@ class Node {
         this.value = value;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 }
 
@@ -16,7 +17,7 @@ class Tree {
     }
 
     insert(value) {
-        if(this.find(value)) return console.log("이미 값이 존재합니다.");
+        if (this.find(value)) return console.log("이미 값이 존재합니다.");
 
         const newNode = new Node(value);
         if (!this.root) return this.root = newNode;
@@ -25,6 +26,8 @@ class Tree {
     }
 
     _insert(node, current) {
+        node.parent = current;
+
         if (node.value < current.value) {
             if (current.left) return this._insert(node, current.left);
             return current.left = node;
@@ -38,13 +41,13 @@ class Tree {
         let current = this.root;
 
         while (current !== undefined) {
-            if(!current) return null;
+            if (!current) return null;
 
             if (current.value === value) return current;
 
             if (value < current.value) {
                 current = current.left;
-            }else{
+            } else {
                 current = current.right;
             }
         }
@@ -52,6 +55,78 @@ class Tree {
         return null;
     }
 
+    erase(value) {
+        const current = this.find(value);
+        if (!current) return console.log("값이 존재하지 않습니다.");
+
+        if (!current.right && !current.left) {
+            if (value < current.parent.value) return current.parent.left = null;
+            return current.parent.right = null;
+        } else if (current.right && current.left) {
+            let child = current.right;
+
+            while(child.left){
+                child = child.left;
+            }
+
+            current.value = child.value;
+            if(child.parent === current) return child.parent.right = null;
+            child.parent.left = null;
+            return
+
+        } else {
+            if (current.left) {
+                current.value = current.left.value;
+                current.left = null;
+                return; 
+            }
+            current.value = current.right.value;
+            current.right = null;
+            return;
+        }
+
+    }
+
+    preorder(){
+        console.log("preorder start");
+        const current = this.root;
+        this._preorder(current);
+    }
+
+    _preorder(node){
+        console.log(node.value);
+        if(node.left) this._preorder(node.left);
+        if(node.right) this._preorder(node.right);
+        return;
+    }
+    
+    inorder(){
+        console.log("inorder start");
+        const current = this.root;
+        this._inorder(current);
+    }
+    
+    _inorder(node){
+        if(node.left) this._inorder(node.left);
+        console.log(node.value);
+        if(node.right) this._inorder(node.right);
+        return;
+        
+    }
+    
+    postorder(){
+        console.log("postorder start");
+        const current = this.root;
+        this._postorder(current);
+    }
+    
+    _postorder(node){
+        if(node.left) this._postorder(node.left);
+        if(node.right) this._postorder(node.right);
+        console.log(node.value);
+        return;
+        
+    }
 
 
 }
@@ -63,6 +138,3 @@ addingArray = [6, 2, 1, 4, 3, 5, 7, 8, 9];
 for (let val of addingArray) {
     tree.insert(val);
 }
-
-tree.insert(8);
-console.log(tree.find(7));
