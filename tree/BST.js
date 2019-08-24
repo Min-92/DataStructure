2d55bc7692e3ec04220e5f0b4e52b29d199c24c9
Min-92 <0201.mj.lee@gmail.com>
2019-08-24 16:53:08 +0900

    feat : find 메소드 추가
    - refactor : insert method

--- tree/BST.js
+++ tree/BST.js
@@ -2,34 +2,54 @@
 // 중복 불가능.
 
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null;
     }
 }
 
-class Tree{
-    constructor(){
+class Tree {
+    constructor() {
         this.root = null;
 
     }
 
-    insert(value){
+    insert(value) {
+        if(this.find(value)) return console.log("이미 값이 존재합니다.");
+
         const newNode = new Node(value);
-        if(!this.root) return this.root = newNode;
-        
-        return this._insert(newNode,this.root);
+        if (!this.root) return this.root = newNode;
+
+        return this._insert(newNode, this.root);
     }
 
-    _insert(node,current){
-        if(node.value < current.value){
-            if(current.left) return this._insert(node,current.left);
+    _insert(node, current) {
+        if (node.value < current.value) {
+            if (current.left) return this._insert(node, current.left);
             return current.left = node;
         }
 
-        if(current.right) return this._insert(node,current.right);
+        if (current.right) return this._insert(node, current.right);
         return current.right = node;
+    }
+
+    find(value) {
+        let current = this.root;
+
+        while (current !== undefined) {
+            if(!current) return null;
+
+            if (current.value === value) return current;
+
+            if (value < current.value) {
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+
+        return null;
     }
 
 
@@ -39,7 +59,10 @@ class Tree{
 
 const tree = new Tree();
 
-addingArray = [6,2,1,4,3,5,7,8,9];
-for(let val of addingArray){
+addingArray = [6, 2, 1, 4, 3, 5, 7, 8, 9];
+for (let val of addingArray) {
     tree.insert(val);
 }
+
+tree.insert(8);
+console.log(tree.find(7));
